da27eb001678eb8f65b536a5fb8f9415a42b4290
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-05-02 22:35:05 +0530

    Revert "changes in cors"
    This reverts commit 1d5eecdb012a7ee526f0f7bafedbc712de04ab05.

--- app.js
+++ app.js
@@ -17,11 +17,9 @@ const locationAccessController = require("./controllers/location-access"); // ne
 
 const app = express();
 const server = http.createServer(app);
-
 const port = process.env.PORT || 5000;
 
-app.use(cors());  
-app.options('*', cors()); // enable preflight for all routes
+app.use(cors());
 app.use(express.json());
 
 // PostgreSQL Database Connection
@@ -79,6 +77,7 @@ app.post("/api/register", signupController.signup);
 app.post("/api/login", loginController.login);
 app.post("/api/forget-password", forgetPasswordController.forgetPassword);
 app.post("/api/reset-password/:token", resetPasswordController.resetPassword);
+
 app.post("/api/token", locationAccessController.generateToken);
 app.post("/api/location", locationAccessController.updateUserLocation);
 app.get("/api/location/:token", locationAccessController.getLocationByToken);
